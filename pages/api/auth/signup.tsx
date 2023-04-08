665b3dc5f63ee4e3bb1163951cc007ee41335585
minchodang <minsu910725@gmail.com>
2023-04-08 20:15:14 +0900

    Fix: uniqueId 로직 추가

--- pages/api/auth/signup.tsx
+++ pages/api/auth/signup.tsx
@@ -22,6 +22,17 @@ const handler = async (req: any, res: any) => {
     const client = await connectToDatabase();
 
     const db = client.db();
+
+    const existingUser = await db.collection('users').findOne({email: email})
+
+    if (existingUser) {
+        res.status(422).json({
+            message: 'User exists already!'
+        })
+        client.close();
+        return
+    }
+
     const hashedPassword = await hashPassword(password)
     await db.collection('users').insertOne({
         email: email,
@@ -30,6 +41,7 @@ const handler = async (req: any, res: any) => {
     res.status(201).json({
         message: 'Created user!'
     })
+    client.close();
 }
 
 
