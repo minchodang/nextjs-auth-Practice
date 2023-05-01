884fb76087b8be96c6081c5472cd15359e03c360
minchodang <minsu910725@gmail.com>
2023-05-01 17:30:56 +0900

    Feat: loginSession 추가

--- components/layout/main-navigation.tsx
+++ components/layout/main-navigation.tsx
@@ -1,24 +1,38 @@
 import Link from 'next/link';
-
 import classes from './main-navigation.module.css';
+import { signOut, useSession } from 'next-auth/client';
+import React from 'react';
 
 function MainNavigation() {
+    const [session, loading] = useSession();
+    const logoutHandler = (e: React.MouseEvent) => {
+        e.preventDefault();
+        signOut({
+            redirect: false,
+        });
+    };
     return (
         <header className={classes.header}>
-            <Link href='/'>
+            <Link href="/">
                 <div className={classes.logo}>Next Auth</div>
             </Link>
             <nav>
                 <ul>
-                    <li>
-                        <Link href='/auth'>Login</Link>
-                    </li>
-                    <li>
-                        <Link href='/profile'>Profile</Link>
-                    </li>
-                    <li>
-                        <button>Logout</button>
-                    </li>
+                    {!session && !loading ? (
+                        <li>
+                            <Link href="/auth">Login</Link>
+                        </li>
+                    ) : null}
+                    {session ? (
+                        <li>
+                            <Link href="/profile">Profile</Link>
+                        </li>
+                    ) : null}
+                    {session ? (
+                        <li>
+                            <button onClick={logoutHandler}>Logout</button>
+                        </li>
+                    ) : null}
                 </ul>
             </nav>
         </header>
