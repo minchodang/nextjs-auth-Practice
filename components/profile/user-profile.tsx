e219d28ff3ced6a294feb44cb58b355288b23670
minchodang <minsu910725@gmail.com>
2023-05-07 10:50:55 +0900

    Update: ssrGuard, authPageGuard

--- components/profile/user-profile.tsx
+++ components/profile/user-profile.tsx
@@ -1,24 +1,7 @@
 import ProfileForm from './profile-form';
 import classes from './user-profile.module.css';
-import { getSession } from 'next-auth/client';
-import { useEffect, useState } from 'react';
 
 const UserProfile = () => {
-    const [isLoading, setIsLoading] = useState(true);
-
-    useEffect(() => {
-        getSession().then((session) => {
-            if (!session) {
-                window.location.href = '/auth';
-            }
-            setIsLoading(false);
-        });
-    }, []);
-
-    if (isLoading) {
-        return <p className={classes.profile}>Loading...</p>;
-    }
-
     return (
         <section className={classes.profile}>
             <h1>Your User Profile</h1>
