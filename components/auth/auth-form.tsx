37c05249b32ec16778f7dcf757776c1e0b35041b
minsuKang <90169703+minchodang@users.noreply.github.com>
2023-05-14 22:44:21 +0900

    Feat/ssr guard (#15)
    * Feat: loginRequest 추가
    
    * Feat: loginSession 추가
    
    * Feat: loginGuard 추가
    
    * Update: ssrGuard, authPageGuard

--- components/auth/auth-form.tsx
+++ components/auth/auth-form.tsx
@@ -1,31 +1,31 @@
-import {useRef, useState} from 'react';
-import classes from './auth-form.module.css'
-import {signIn} from "next-auth/client";
-
+import { useRef, useState } from 'react';
+import classes from './auth-form.module.css';
+import { signIn } from 'next-auth/client';
+import { useRouter } from 'next/router';
 
 const createUser = async (email: string, password: string) => {
     const response = await fetch('/api/auth/signup', {
         method: 'POST',
         body: JSON.stringify({
-            email, password
-
+            email,
+            password,
         }),
         headers: {
-            'Content-Type': 'application/json'
-        }
-    })
+            'Content-Type': 'application/json',
+        },
+    });
     const data = await response.json();
     if (!response.ok) {
-        throw new Error(data.message || 'Something  went wrong!')
+        throw new Error(data.message || 'Something  went wrong!');
     }
     return data;
-}
-
+};
 
 function AuthForm() {
+    const emailInputRef = useRef<HTMLInputElement>(null);
+    const passwordInputRef = useRef<HTMLInputElement>(null);
+    const router = useRouter();
     const [isLogin, setIsLogin] = useState(true);
-    const emailInputRef = useRef<HTMLInputElement>(null)
-    const passwordInputRef = useRef<HTMLInputElement>(null)
 
     function switchAuthModeHandler() {
         setIsLogin((prevState) => !prevState);
@@ -34,7 +34,7 @@ function AuthForm() {
     const submitHandler = async (event: any) => {
         event.preventDefault();
         if (!emailInputRef.current || !passwordInputRef.current) {
-            return
+            return;
         }
         const enteredEmail = emailInputRef.current.value;
         const enteredPassword = passwordInputRef.current.value;
@@ -43,39 +43,38 @@ function AuthForm() {
             const result = await signIn('credentials', {
                 redirect: false,
                 email: enteredEmail,
-                password: enteredPassword
-            })
+                password: enteredPassword,
+            });
+            if (!result?.error) {
+                router.replace('/profile');
+            }
 
-            console.log(result, '리절트!!')
         } else {
             try {
-
-                const result = await createUser(enteredEmail, enteredPassword)
-                console.log(result)
-
+                const result = await createUser(enteredEmail, enteredPassword);
+                console.log(result);
             } catch (error) {
-                console.log(error)
+                console.log(error);
             }
         }
-
-    }
+    };
 
     return (
         <section className={classes.auth}>
             <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
             <form onSubmit={submitHandler}>
                 <div className={classes.control}>
-                    <label htmlFor='email'>Your Email</label>
-                    <input type='email' id='email' required ref={emailInputRef}/>
+                    <label htmlFor="email">Your Email</label>
+                    <input type="email" id="email" required ref={emailInputRef} />
                 </div>
                 <div className={classes.control}>
-                    <label htmlFor='password'>Your Password</label>
-                    <input type='password' id='password' required ref={passwordInputRef}/>
+                    <label htmlFor="password">Your Password</label>
+                    <input type="password" id="password" required ref={passwordInputRef} />
                 </div>
                 <div className={classes.actions}>
                     <button>{isLogin ? 'Login' : 'Create Account'}</button>
                     <button
-                        type='button'
+                        type="button"
                         className={classes.toggle}
                         onClick={switchAuthModeHandler}
                     >
