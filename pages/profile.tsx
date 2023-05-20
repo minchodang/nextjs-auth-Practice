1e6b4ed44820faf53c5df19324be067ec51e90bf
minsuKang <90169703+minchodang@users.noreply.github.com>
2023-05-20 09:25:16 +0900

    Develop (#16)
    * Feat: loginGuard 추가 (#13)
    
    * Feat: loginRequest 추가
    
    * Feat: loginSession 추가
    
    * Feat: loginGuard 추가
    
    * Feat/ssr guard (#15)
    
    * Feat: loginRequest 추가
    
    * Feat: loginSession 추가
    
    * Feat: loginGuard 추가
    
    * Update: ssrGuard, authPageGuard

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,7 +1,26 @@
 import UserProfile from '../components/profile/user-profile';
+import { getSession } from 'next-auth/client';
 
 function ProfilePage() {
-  return <UserProfile />;
+    return <UserProfile />;
+}
+
+export async function getServerSideProps(context: { req: any }) {
+    const session = await getSession({ req: context.req });
+
+    if (!session) {
+        return {
+            redirect: {
+                destination: '/auth',
+                permanent: false,
+            },
+        };
+    }
+    return {
+        props: {
+            session,
+        },
+    };
 }
 
 export default ProfilePage;
