bc433340e06317723b408e0664e23340a0cae9d5
minsuKang <minsu910725@gmail.com>
2023-06-07 14:25:25 +0900

    Feat: passwordChange 로직 추가

--- components/profile/profile-form.tsx
+++ components/profile/profile-form.tsx
@@ -1,21 +1,49 @@
+import { FormEvent, useRef } from 'react';
 import classes from './profile-form.module.css';
+import { PasswordDataType } from './user-profile';
 
-function ProfileForm() {
-  return (
-    <form className={classes.form}>
-      <div className={classes.control}>
-        <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
-      </div>
-      <div className={classes.control}>
-        <label htmlFor='old-password'>Old Password</label>
-        <input type='password' id='old-password' />
-      </div>
-      <div className={classes.action}>
-        <button>Change Password</button>
-      </div>
-    </form>
-  );
-}
+type ProfileFormProps = {
+    onChangePassword: (passwordData: PasswordDataType) => void;
+};
+
+const ProfileForm = ({ onChangePassword }: ProfileFormProps) => {
+    const newPasswordRef = useRef<HTMLInputElement>(null);
+    const oldPasswordRef = useRef<HTMLInputElement>(null);
+    const submitHandler = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        if (!oldPasswordRef.current || !newPasswordRef.current) {
+            return;
+        }
+        const enteredOldPassword = oldPasswordRef.current.value;
+
+        const enteredNewPassword = newPasswordRef.current.value;
+        onChangePassword({
+            oldPassword: enteredOldPassword,
+            newPassword: enteredNewPassword,
+        });
+    };
+
+    return (
+        <form
+            className={classes.form}
+            onSubmit={(event) => {
+                submitHandler(event);
+            }}
+        >
+            <div className={classes.control}>
+                <label htmlFor="new-password">New Password</label>
+                <input type="password" id="new-password" ref={newPasswordRef} />
+            </div>
+            <div className={classes.control}>
+                <label htmlFor="old-password">Old Password</label>
+                <input type="password" id="old-password" ref={oldPasswordRef} />
+            </div>
+            <div className={classes.action}>
+                <button>Change Password</button>
+            </div>
+        </form>
+    );
+};
 
 export default ProfileForm;
