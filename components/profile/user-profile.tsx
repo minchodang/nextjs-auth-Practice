c7a3397d4b4a5eab64e14e34a6ec158b2775057d
minsuKang <90169703+minchodang@users.noreply.github.com>
2023-06-12 11:15:25 +0900

    Develop (#25)
    * Feat: loginGuard 추가 (#13)
    
    * Feat: loginRequest 추가
    
    * Feat: loginSession 추가
    
    * Feat: loginGuard 추가
    
    * Feat/ssr guard (#15)
    
    * Feat: loginRequest 추가
    
    * Feat: loginSession 추가
    
    * Feat: loginGuard 추가
    
    * Update: ssrGuard, authPageGuard
    
    * Feature/session component (#18)
    
    * Feat: loginRequest 추가
    
    * Feat: loginSession 추가
    
    * Feat: loginGuard 추가
    
    * Update: ssrGuard, authPageGuard
    
    * Fix: session add
    
    * Feat:api-route-guard 추가 (#20)
    
    * Feat: passwordLogic add (#22)
    
    * Feature/password change (#24)
    
    * Feat: passwordLogic add
    
    * Feat: passwordChange 로직 추가

--- components/profile/user-profile.tsx
+++ components/profile/user-profile.tsx
@@ -1,11 +1,28 @@
 import ProfileForm from './profile-form';
 import classes from './user-profile.module.css';
 
+export type PasswordDataType = {
+    oldPassword: string;
+    newPassword: string;
+};
+
 const UserProfile = () => {
+    const changePasswordHandler = async (passwordData: PasswordDataType) => {
+        const response = await fetch('/api/user/change-password', {
+            method: 'PATCH',
+            body: JSON.stringify(passwordData),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        const data = await response.json();
+        console.log(data);
+    };
+
     return (
         <section className={classes.profile}>
             <h1>Your User Profile</h1>
-            <ProfileForm />
+            <ProfileForm onChangePassword={changePasswordHandler} />
         </section>
     );
 };
